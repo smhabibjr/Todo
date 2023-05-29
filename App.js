717a7647ddacfc22162d602c79ca07a76dd19bd8
HabibJR <smhabib.abir2030@gmail.com>
2023-05-29 14:10:00 +0200

    add header component

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "bud coffe", key: 0},
+    { text: "bud coffe", key: 1 },
+    { text: "bud coffe", key: 3 }
+  ])
   return (
     <View style={styles.container}>
-     
+     {/* header  */}
+     <Header />
+
+      <View style={styles.content}> 
+        {/* to do form  */}
+        <View style={styles.list}> 
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <Text> {item.text} </Text>
+            )}
+            />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,4 +34,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  content: {
+    padding:40
+  },
+  list: {
+    marginTop: 20
+  }
 });
