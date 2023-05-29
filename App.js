94244f0fbcf05b17a2178db11fb657285615eb81
HabibJR <smhabib.abir2030@gmail.com>
2023-05-29 16:28:09 +0200

    add delete functionality

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoitem';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: "bud coffe", key: 0},
-    { text: "bud coffe", key: 1 },
-    { text: "bud coffe", key: 3 }
+    { text: "create a react native app", key: 1 },
+    { text: "create web using ruby on rails ", key: 3 }
   ])
+
+  const pressHandler = (key) => {
+    setTodos((prevItem) => {
+      return prevItem.filter(todo => todo.key != key);
+    })
+  }
+
   return (
     <View style={styles.container}>
      {/* header  */}
@@ -20,7 +28,7 @@ export default function App() {
           <FlatList 
             data={todos}
             renderItem={({item}) => (
-              <Text> {item.text} </Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
             />
         </View>
